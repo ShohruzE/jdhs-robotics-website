9603d4edf202b65c98a69b08733812fb1a5b37f2
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-06-07 19:53:33 -0400

    Configure font with next project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from 'next/font/local';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const bahnschrift = localFont({ src: "../../public/fonts/Bahnschrift-Font-Family/Bahnschrift.ttf" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${bahnschrift.className} ${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
