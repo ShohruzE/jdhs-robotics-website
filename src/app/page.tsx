1719b8fbe4aa12a28d09541bb3a44c67aaa90c19
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-06-15 10:39:10 -0400

    Add hero component to render to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 
+import Hero from "../components/Hero";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Hero />
+    </div>
   );
 }
