9da93e2d00fe8b1aa82c853ecb01672acec9f0c2
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-06-16 19:11:30 -0400

    Add SponsorBar component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 import Hero from "../components/Hero";
+import SponsorBar from "../components/SponsorBar";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <SponsorBar />
     </div>
   );
 }
