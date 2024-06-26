9a9e69483c84ee0f8553ab7caf1ba892c8396027
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-06-25 23:31:17 -0400

    Add data for each image to allow for dynamic styling based on the current slide

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,62 @@ import React from 'react'
 import hero1 from '../../public/images/hero-1.png'
 import hero2 from '../../public/images/hero-2.png'
 import hero3 from '../../public/images/hero-3.png'
+import logoWithText from '../../public/images/2024-frc-333-logo-with-text.png'
+import hammerheadHeroLogo from '../../public/images/hammer-head-logo-transparent.png';
+import sponsorHeroText from '../../public/images/sponsor-hero-text.png';
 
 import Carousel from './ui/Carousel'
 
 const Hero = () => {
 
-  const IMAGES = [hero1, hero2, hero3];
+  const IMAGES = [
+    {
+        "imageSrc": hero1,
+        "styles": {
+          "format": "container mx-auto flex flex-col items-end gap-12",
+          "imageHeaderWidth": 568,
+          "imageHeaderHeight": 225,
+          "captionStyles": "flex flex-col items-end gap-9"
+        },
+        "content": {
+            "imageHeaderSrc": logoWithText,
+            "title": "",
+            "description": "Driven by passion. Powered by innovation.",
+            "buttonText": "Learn More",
+            "buttonLink": "/about"
+        }
+    },
+    {
+        "imageSrc": hero2,
+        "styles": {
+          "format": "container mx-auto flex flex-col items-start gap-12",
+          "imageHeaderWidth": 480,
+          "imageHeaderHeight": 272,
+          "captionStyles": "flex flex-col items-start gap-9 max-w-[475px]"
+        },
+        "content": {
+            "imageHeaderSrc": hammerheadHeroLogo,
+            "description": "Meet the team's 2024 robot, the culmination of a season full of trials and triumphs. Hammerhead takes the stage, boasting a unique pivot-mounted trolley system.",
+            "buttonText": "View Robot",
+            "buttonLink": "/robots/crescendo2024"
+        }
+    },
+    {
+        "imageSrc": hero3,
+        "styles": {
+          "format": "container mx-auto flex flex-col items-start gap-6",
+          "imageHeaderWidth": 1010,
+          "imageHeaderHeight": 186,
+          "captionStyles": "flex flex-col items-start gap-9 max-w-[650px]"
+        },
+        "content": {
+            "imageHeaderSrc": sponsorHeroText,
+            "description": "Become part of an inclusive community where passion, collaboration, and competitive excellence define the next generation of STEAM leaders.",
+            "buttonText": "Connect With Us",
+            "buttonLink": "/about"
+        }
+    }
+  ]
 
   return (
     <div>
