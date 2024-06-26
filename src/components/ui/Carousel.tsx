1e224bf2981fd1f0f8b530ca65f6a9cc086f7552
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-06-25 23:31:57 -0400

    Add support for text overlay to hero images and add left and right buttons

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -2,12 +2,31 @@
 
 import React, { useState, useEffect } from 'react'
 import Image, { StaticImageData } from 'next/image';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 type CarouselProps = {
-  images: StaticImageData[]
+  images: ImageProps[]
   autoSlide?: boolean
   autoSlideInterval?: number
+}
+
+type ImageProps = {
+  imageSrc: StaticImageData
+  styles: {
+    format: string
+    imageHeaderWidth?: number | undefined
+    imageHeaderHeight?: number | undefined
+    titleStyles?: string | undefined
+    captionStyles: string
+  }
+  content: {
+    imageHeaderSrc?: StaticImageData | undefined
+    title?: string
+    description: string
+    buttonText: string
+    buttonLink: string
+  }
 }
 
 const Carousel = ({ images, autoSlide = false, autoSlideInterval = 5000 }: CarouselProps) => {
@@ -45,37 +64,65 @@ const Carousel = ({ images, autoSlide = false, autoSlideInterval = 5000 }: Carou
     <div className="w-full h-screen relative">
       <div className="w-full h-screen flex overflow-hidden">
         {images.map((image, index) => (
-          <Image 
-            key={index}
-            src={image.src}
-            width={image.width}
-            height={image.height}
-            alt={`carousel hero image ${index + 1}`}
+          <div
+            key={index} 
             className={`w-full h-screen object-cover block shrink-0 grow-0 transition duration-1000 ease-in-out delay-200`}
             style={{ transform: `translateX(-${imageIndex * 100}%)`}}
-          />
+          >
+            <Image 
+              key={index}
+              src={image.imageSrc}
+              alt={`carousel hero image ${index + 1}`}
+              className="w-full h-screen object-cover"
+            />
+            <div className="absolute inset-0 flex items-center">
+              <div className={`${image.styles.format}`}>
+                {image.content.imageHeaderSrc && (
+                  <Image 
+                    src={image.content.imageHeaderSrc}
+                    width={image.styles.imageHeaderWidth || 568}
+                    height={image.styles.imageHeaderHeight || 225}
+                    alt="logo with text"
+                  />
+                )}
+                {image.content.title && (
+                  <h1 className={`${image.styles.titleStyles}`}>{image.content.title}</h1>
+                )}
+                <div className={`${image.styles.captionStyles}`}>
+                  <p className="font-light text-2xl text-secondary">{image.content.description}</p>
+                  <button className="bg-secondary text-primary font-bold text-xl px-12 py-1 rounded-lg hover:bg-accent">
+                    <Link href={`${image.content.buttonLink}`}>{image.content.buttonText}</Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
-      <button 
-        onClick={previousImage}
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-secondary text-primary px-2 py-1 rounded-lg hover:bg-primary">
-          <ArrowLeft size={24} />
-      </button>
-      <button 
-        onClick={nextImage}
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-secondary text-primary px-2 py-1 rounded-lg hover:bg-primary">
-          <ArrowRight size={24} />
-      </button>
-      <div className="absolute bottom-[1px] left-1/2 -translate-x-1/2 flex gap-4">
-        {images.map((_, index) => (
-          <button 
-            key={index}
-            onClick={() => setImageIndex(index)}
-            onMouseEnter={() => clearInterval(autoSlideInterval)}
-            className={`w-40 h-3 bg-[rgb(255,255,255,0.5)] hover:bg-secondary transition-all ${imageIndex === index ? "bg-secondary" : ""}`}
-          />
-        ))}
-      </div>
+        <button 
+          onClick={previousImage}
+          className="group overflow-hidden absolute top-1/2 left-0 transform -translate-y-1/2 bg-secondaryOpaque text-tertiary h-44 w-7 rounded-tr-lg rounded-br-lg transition-all duration-300 hover:bg-secondary hover:w-14">
+            <div className="relative left-0 transition-all duration-300 group-hover:left-5">
+              <ChevronLeft size={35} strokeWidth={3} />
+            </div>
+        </button>
+        <button 
+          onClick={nextImage}
+          className="overflow-hidden absolute top-1/2 right-0 transform -translate-y-1/2 bg-secondaryOpaque text-tertiary h-44 w-7 rounded-tl-lg rounded-bl-lg transition-all duration-300 hover:bg-secondary hover:w-14">
+            <div>
+              <ChevronRight size={35} strokeWidth={3} />
+            </div>
+        </button>
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex gap-4">
+          {images.map((_, index) => (
+            <button 
+              key={index}
+              onClick={() => setImageIndex(index)}
+              onMouseEnter={() => clearInterval(autoSlideInterval)}
+              className={`w-40 h-3 ${imageIndex == index ? "bg-secondary" : "bg-secondaryOpaque"} hover:bg-secondary transition duration-1000 ease-in-out delay-200`}
+            />
+          ))}
+        </div>
     </div>
   )
 }
